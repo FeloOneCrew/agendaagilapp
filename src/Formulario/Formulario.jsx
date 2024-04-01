abd1748b2a8f96136f8e080bc799664331226911
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-01 17:51:31 -0500

    funcion para ingresar y cargar datos del formulario

--- src/Formulario/Formulario.jsx
+++ src/Formulario/Formulario.jsx
@@ -5,11 +5,12 @@ import {useState} from 'react' //Para crear variables de estado dentro del compo
 
 export function Formulario(){
 
-    const[verCedula, modificarCedula] = useState("")
-    const[verPassword, modificarPassword] = useState("")
+    const[verCedula, guardarCedula] = useState("")
+    const[verPassword, guardarPassword] = useState("")
     
-    function procesarFormulario(){
-        
+    function procesarFormulario(evento){
+        evento.preventDefault()
+        console.log("Le hicieron clic al boton del formulario")
     }
 
     return(
@@ -18,7 +19,11 @@ export function Formulario(){
                 <div className="row justify-content-center text-center">
                     <div className="col12 col-md-4">
                         <img src="../../src/assets/logoSura.png" alt="logo de sura" className="img-fluid" />
-                        <form action="" className="border rounded p-2">
+                        <form 
+                            action="" 
+                            className="border rounded p-2"
+                            onSubmit={procesarFormulario}
+                        >
                             <h2>Gestiona tus citas</h2>
                             <div class="input-group mb-3 mt-5">
                                 <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-vcard"></i></span>
@@ -27,6 +32,9 @@ export function Formulario(){
                                     class="form-control" 
                                     placeholder="Número de cédula"
                                     id="identificacion"
+                                    onChange={function(evento){
+                                        guardarCedula(evento.target.value)
+                                    }}
                                 />
                             </div>
 
@@ -35,8 +43,11 @@ export function Formulario(){
                                 <input 
                                     type="text" 
                                     class="form-control" 
-                                    placeholder="Password"
+                                    placeholder="contraseña"
                                     id="contraseña"
+                                    onChange={function(evento){
+                                        guardarPassword(evento.target.value)
+                                    }}
                                 />
                             </div>
                             <button type="submit" className="btn mt-3 colorbtn">Ingresar</button>
